c6ec7af50c4c9bfab39763b2133983c3f195d3c2
Idzanaagi <anmorgunowa@gmail.com>
2023-04-24 17:10:47 +0300

    feat: add Login page description

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -1,7 +1,9 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.FindBy;
 
 public class Login {
 
@@ -10,6 +12,40 @@ public class Login {
     public Login(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "username")
+    private WebElement username;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "formly_1_input_username_0")
+    private WebElement formlyUsername;
+
+    @FindBy(className = "btn")
+    private WebElement btnLogin;
+
+    @FindBy(xpath = "//*[@id=\"username\"]/following::div[1]/div")
+    private WebElement errorUsername;
+
+    @FindBy(xpath = "//*[@id=\"password\"]/following::div[1]/div")
+    private WebElement errorPassword;
+
+    public void fillUsername(String value) {
+        username.sendKeys(value);
+    }
+
+    public void fillPassword(String value) {
+        password.sendKeys(value);
+    }
+
+    public void fillFormlyUsername(String value) {
+        formlyUsername.sendKeys(value);
+    }
+
+    public void clickLoginBtn() {
+        btnLogin.click();
     }
 
     public void open() {

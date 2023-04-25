438fe00e61272f2308bcafdd0aaaa949fa2e535e
Idzanaagi <anmorgunowa@gmail.com>
2023-04-25 13:30:05 +0300

    feat: add pages description

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -1,11 +1,14 @@
 package pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.FindBy;
 
+
 public class Login {
+
 
     WebDriver driver;
 
@@ -14,42 +17,71 @@ public class Login {
         PageFactory.initElements(driver, this);
     }
 
+
     @FindBy(id = "username")
-    private WebElement username;
+    private WebElement usernameField;
 
     @FindBy(id = "password")
-    private WebElement password;
+    private WebElement passwordField;
 
     @FindBy(id = "formly_1_input_username_0")
-    private WebElement formlyUsername;
+    private WebElement formlyUsernameField;
 
     @FindBy(className = "btn")
     private WebElement btnLogin;
 
     @FindBy(xpath = "//*[@id=\"username\"]/following::div[1]/div")
-    private WebElement errorUsername;
+    private WebElement usernameErrorMessage;
 
     @FindBy(xpath = "//*[@id=\"password\"]/following::div[1]/div")
-    private WebElement errorPassword;
+    private WebElement passwordErrorMessage;
+
+    @FindBy(xpath = "/html/body/div[3]/div/div/div/div[2]")
+    private WebElement failedLoginErrorMessage;
+
 
     public void fillUsername(String value) {
-        username.sendKeys(value);
+        usernameField.sendKeys(value);
     }
 
     public void fillPassword(String value) {
-        password.sendKeys(value);
+        passwordField.sendKeys(value);
     }
 
     public void fillFormlyUsername(String value) {
-        formlyUsername.sendKeys(value);
+        formlyUsernameField.sendKeys(value);
     }
 
     public void clickLoginBtn() {
         btnLogin.click();
     }
 
-    public void open() {
-        driver.get("https://www.way2automation.com/angularjs-protractor/registeration/#/login");
+    public String getFailedLoginMessage() {
+        return failedLoginErrorMessage.getText();
+    }
+
+    public String getAttributeBtnLogin() {
+        return btnLogin.getAttribute("disabled");
+    }
+
+    public void removeFocusFromLastField() {
+        formlyUsernameField.sendKeys(Keys.TAB);
+    }
+
+    public String getUsernameErrorMessage() {
+        return usernameErrorMessage.getText();
+    }
+
+    public String getPasswordErrorMessage() {
+        return passwordErrorMessage.getText();
+    }
+
+    public String getUsernameInputValue() {
+        return usernameField.getAttribute("value");
+    }
+
+    public String getPasswordInputValue() {
+        return passwordField.getAttribute("value");
     }
 
 }

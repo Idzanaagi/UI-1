b90029f3743a28b184bb24b0e1eb859fc70373c2
Idzanaagi <anmorgunowa@gmail.com>
2023-05-30 12:01:56 +0300

    feat: add tabs test

---
+++ src/test/java/tests/TabsTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import base.BasePage;
+import base.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import pages.DefaultHtmlPage;
+import pages.TabsPage;
+import utils.TabsUtils;
+
+
+/**
+ * The type Tabs test.
+ */
+@Epic("Tabs")
+public class TabsTest extends BaseTest {
+
+    /**
+     * Tabs.
+     */
+    @Story("User opens tabs in new windows")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    public void tabs() throws Exception {
+        TabsPage tabsPage = new TabsPage(getDriver());
+        BasePage basePage = new BasePage(getDriver());
+        TabsUtils tabsUtils = new TabsUtils();
+        DefaultHtmlPage defaultHtmlPage = new DefaultHtmlPage(getDriver());
+        basePage.launch("http://way2automation.com/way2auto_jquery/frames-and-windows.php");
+        tabsUtils.switchFrame(getDriver(), 0);
+        tabsPage.clickNewTabLink();
+        tabsUtils.switchTab(getDriver(), 1);
+        defaultHtmlPage.clickNewTabLink();
+        tabsUtils.switchTab(getDriver(), 2);
+        Assertions.assertEquals("https://way2automation.com/way2auto_jquery/frames-windows/defult1.html#",
+                getDriver().getCurrentUrl(), "expected and received url did not match");
+        Assertions.assertEquals(3, getDriver().getWindowHandles().size(),
+                "expected and received number of opened tabs will not match");
+    }
+}
---
+++ src/test/java/utils/TabsUtils.java
@@ -0,0 +1,33 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * The type Tabs utils.
+ */
+public class TabsUtils {
+
+    /**
+     * Switch tab.
+     * @param driver the driver
+     * @param tabIndex the tab index
+     */
+    public void switchTab(final WebDriver driver, final int tabIndex) throws Exception {
+        if (driver.getWindowHandles().size() >= tabIndex) {
+            driver.switchTo().window(driver.getWindowHandles().toArray()[tabIndex].toString());
+        }
+        else {
+            throw new Exception("Trying to open a tab that does not exist");
+        }
+    }
+
+    /**
+     * Switch frame.
+     * @param driver     the driver
+     * @param frameIndex the frame index
+     */
+    public void switchFrame(final WebDriver driver, final int frameIndex) {
+        driver.switchTo().frame(frameIndex);
+    }
+}
